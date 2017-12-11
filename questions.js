8998a439ed0b3963140834bf95306d3730e31d0c
Truong <kevin.truong@yale.edu>
2017-12-11 05:10:28 -0500

    First Commit

---
+++ questions.js
@@ -0,0 +1,99 @@
+var questions = [{
+	"question": "What",
+	"option1": "R",
+	"option2": "K",
+	"option3": "R",
+	"option4": "K",
+	"answer":"1"
+},{
+	"question": "What",
+	"option1": "R",
+	"option2": "K",
+	"option3": "R",
+	"option4": "K",
+	"answer":"1"
+}, {
+	"question": "What",
+	"option1": "R",
+	"option2": "K",
+	"option3": "R",
+	"option4": "K",
+	"answer":"1"
+},{
+	"question": "What",
+	"option1": "R",
+	"option2": "K",
+	"option3": "R",
+	"option4": "K",
+	"answer":"1"
+},{
+	"question": "What",
+	"option1": "R",
+	"option2": "K",
+	"option3": "R",
+	"option4": "K",
+	"answer":"1"
+},{
+	"question": "What",
+	"option1": "R",
+	"option2": "K",
+	"option3": "R",
+	"option4": "K",
+	"answer":"1"
+},{
+	"question": "What",
+	"option1": "R",
+	"option2": "K",
+	"option3": "R",
+	"option4": "K",
+	"answer":"1"
+},{
+	"question": "What",
+	"option1": "R",
+	"option2": "K",
+	"option3": "R",
+	"option4": "K",
+	"answer":"1"
+},{
+	"question": "What",
+	"option1": "R",
+	"option2": "K",
+	"option3": "R",
+	"option4": "K",
+	"answer":"1"
+},{
+	"question": "What",
+	"option1": "R",
+	"option2": "K",
+	"option3": "R",
+	"option4": "K",
+	"answer":"1"
+},{
+	"question": "What",
+	"option1": "R",
+	"option2": "K",
+	"option3": "R",
+	"option4": "K",
+	"answer":"1"
+},{
+	"question": "What",
+	"option1": "R",
+	"option2": "K",
+	"option3": "R",
+	"option4": "K",
+	"answer":"1"
+},{
+	"question": "What",
+	"option1": "R",
+	"option2": "K",
+	"option3": "R",
+	"option4": "K",
+	"answer":"1"
+},{
+	"question": "What",
+	"option1": "R",
+	"option2": "K",
+	"option3": "R",
+	"option4": "K",
+	"answer":"1"
+}];
